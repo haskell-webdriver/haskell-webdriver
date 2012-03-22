11626df315120d2f889412729bcc61af11f37868
Kallisti Development <acurtis@spsu.edu>
2012-03-21 23:34:32 -0400

    export Test.WebDriver.Commands.Wait

--- Test/WebDriver.hs
+++ Test/WebDriver.hs
@@ -1,7 +1,9 @@
 module Test.WebDriver
        ( module Test.WebDriver.Types
        , module Test.WebDriver.Commands
+       , module Test.WebDriver.Commands.Wait
        ) where
 
 import Test.WebDriver.Types
 import Test.WebDriver.Commands
+import Test.WebDriver.Commands.Wait
