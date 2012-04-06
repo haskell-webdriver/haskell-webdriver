18643770db4dfde0d302f1659a5eb1c658663e95
Adam Curtis <acurtis@spsu.edu>
2012-04-06 01:11:24 -0400

    switch to a more lenient base64 decoder to avoid padding errors

--- Test/WebDriver/Utils.hs
+++ Test/WebDriver/Utils.hs
@@ -1,12 +1,9 @@
 module Test.WebDriver.Utils where
 import Test.WebDriver.Types (ServerError(..))
-import Data.ByteString
+import Data.ByteString.Char8
 import Data.ByteString.Base64
 import Control.Exception.Lifted
+import Debug.Trace
 
 b64Decode :: ByteString -> ByteString
-b64Decode = either 
-              (throw . ServerError
-              . ("Error decoding base64-encoded PNG screenshot from server: " 
-                 ++))
-              id . decode
+b64Decode = decodeLenient
