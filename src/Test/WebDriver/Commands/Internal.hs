134705a9bb4594296f9aea8122ac9e7174b14355
Adam Curtis <acurtis@spsu.edu>
2012-06-16 00:00:21 -0400

    fix documentation error

--- src/Test/WebDriver/Commands/Internal.hs
+++ src/Test/WebDriver/Commands/Internal.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings, DeriveDataTypeable, GeneralizedNewtypeDeriving  #-}
 {-# OPTIONS_HADDOCK not-home #-}
--- |Internal functions used to implement the functions exported by "Test.WebDriver"
--- . These may be useful for implementing non-standard webdriver commands.
+-- |Internal functions used to implement the functions exported by 
+-- "Test.WebDriver.Commands". These may be useful for implementing non-standard 
+-- webdriver commands.
 module Test.WebDriver.Commands.Internal 
        (-- * Low-level webdriver functions
          doCommand, doSessCommand, SessionId(..)
