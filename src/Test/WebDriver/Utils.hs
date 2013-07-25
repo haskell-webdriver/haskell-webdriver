6164bf8120e848dbbc193b40d277891f99d238d7
Adam Curtis <acurtis@spsu.edu>
2013-07-25 14:55:21 -0400

    add Test.WebDriver.Utils module which currently only contains urlEncode

---
+++ src/Test/WebDriver/Utils.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Test.WebDriver.Utils where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Network.HTTP.Base as HTTP
+
+urlEncode :: Text -> Text
+urlEncode = T.pack . HTTP.urlEncode . T.unpack 
