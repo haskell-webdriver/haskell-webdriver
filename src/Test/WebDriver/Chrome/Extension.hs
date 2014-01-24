d28da0465b40d657de885864411e097b92d8d58a
Adam Curtis <acurtis@spsu.edu>
2014-01-24 11:20:20 -0500

    represent ChromeExtension with Text instead of ByteString

--- src/Test/WebDriver/Chrome/Extension.hs
+++ src/Test/WebDriver/Chrome/Extension.hs
@@ -7,13 +7,15 @@ module Test.WebDriver.Chrome.Extension
        ) where
 import Data.ByteString.Lazy as LBS
 import Data.ByteString.Base64.Lazy as B64
+import Data.Text.Lazy
+import Data.Text.Lazy.Encoding (decodeLatin1)
 import Data.Aeson
 import Control.Applicative
 import Control.Monad.Base
 
 -- |An opaque type representing a Google Chrome extension. Values of this type
 -- are passed to the 'Test.Webdriver.chromeExtensions' field.
-newtype ChromeExtension = ChromeExtension ByteString
+newtype ChromeExtension = ChromeExtension Text
                         deriving (Eq, Show, Read, ToJSON, FromJSON)
 
 -- |Load a .crx file as a 'ChromeExtension'.
@@ -22,4 +24,4 @@ loadExtension path = liftBase $ loadRawExtension <$> LBS.readFile path
 
 -- |Load raw .crx data as a 'ChromeExtension'.
 loadRawExtension :: ByteString -> ChromeExtension
-loadRawExtension = ChromeExtension . B64.encode
+loadRawExtension = ChromeExtension . decodeLatin1 . B64.encode
