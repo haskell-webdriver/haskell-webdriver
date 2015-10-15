fc4da88787dfa698a7302c8d3c84fd6e04b810ce
Adam Curtis <acurtis@spsu.edu>
2015-10-14 20:26:25 -0400

    update cabal file and improve search-baidu tests

--- test/search-baidu.hs
+++ test/search-baidu.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Control.Monad (void)
-import           Data.List
-import qualified Data.Text                    as T
-import           Test.WebDriver
-import           Test.WebDriver.Commands.Wait
+import Control.Monad
+import Control.Applicative
+import Data.List
+import qualified Data.Text as T
+import Test.WebDriver
+import Test.WebDriver.Commands.Wait
+
+import Prelude
 
 chromeConf = useBrowser chrome defaultConfig
 ffConf = defaultConfig
@@ -13,24 +16,35 @@ ffConf = defaultConfig
 -- Have no fun with baidu but only cause it is loading fast than google in China.
 --
 baidu :: WD ()
-baidu = openPage "http://www.baidu.com/"
+baidu = do 
+  openPage "http://www.baidu.com/"
+  waitUntil 15 $
+    expect . (== "http://www.baidu.com/") =<< getCurrentURL
 
-searchBaidu :: WD Bool
+searchBaidu :: WD ()
 searchBaidu = do
   searchBox <- findElem (ById "kw")
   sendKeys "Cheese!" searchBox
   submit searchBox
-  setImplicitWait 50
-  waitUntil 2000 $ do
+  waitUntil 15 $ do
     title <- getTitle
-    return ("cheese!" `T.isSuffixOf` title)
+    expect ("Cheese!" `T.isInfixOf` title)
+
+  container <- findElem (ById "container")
+  eList1 <- findElems (ByCSS "c-container")
+  eList2 <- findElems (ByClass "c-container")
+  expect =<< (fmap and $ zipWithM (<==>) eList1 eList2)
+
+  forM_ eList1 $ \e -> findElemsFrom e (ByTag "a")
+
+
 
 testCase c = void . runSession c . finallyClose $ (baidu >> searchBaidu)
 
-testSuits = mapM_ testCase  [ffConf, chromeConf]
+testSuites = mapM_ testCase  [ffConf, chromeConf]
 
 main = do
   --testCase capsChrome `seq` testCase capsFF
   --mapM_ (forkOS . testCase) [capsFF, capsChrome]
-  testSuits
+  testSuites
   print "done"
