90ded63218da17fc0bd9f9b208b0b3f60b135757
George Thomas <georgefsthomas@gmail.com>
2021-12-28 00:26:56 +0000

    Update to Aeson 2

--- src/Test/WebDriver/Exceptions/Internal.hs
+++ src/Test/WebDriver/Exceptions/Internal.hs
@@ -15,7 +15,6 @@ import Data.Aeson.Types (Parser, typeMismatch)
 import Data.ByteString.Lazy.Char8 (ByteString)
 import Data.CallStack
 import qualified Data.List as L
-import Data.Text (Text)
 import qualified Data.Text.Lazy.Encoding as TLE
 
 import Control.Applicative
@@ -174,9 +173,9 @@ instance FromJSON FailedCommandInfo where
                       <*> (fmap TLE.encodeUtf8 <$> opt "screen" Nothing)
                       <*> opt "class"      Nothing
                       <*> (catMaybes <$> opt "stackTrace" [])
-    where req :: FromJSON a => Text -> Parser a
+    where req :: FromJSON a => Key -> Parser a
           req = (o .:)            --required key
-          opt :: FromJSON a => Text -> a -> Parser a
+          opt :: FromJSON a => Key -> a -> Parser a
           opt k d = o .:?? k .!= d --optional key
   parseJSON v = typeMismatch "FailedCommandInfo" v
 
@@ -185,9 +184,9 @@ instance FromJSON StackFrame where
                                     <*> reqStr "className"
                                     <*> reqStr "methodName"
                                     <*> req    "lineNumber"
-    where req :: FromJSON a => Text -> Parser a
+    where req :: FromJSON a => Key -> Parser a
           req = (o .:) -- all keys are required
-          reqStr :: Text -> Parser String
+          reqStr :: Key -> Parser String
           reqStr k = req k >>= maybe (return "") return
   parseJSON v = typeMismatch "StackFrame" v
 
