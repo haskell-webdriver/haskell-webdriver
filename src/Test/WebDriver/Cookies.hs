d4e3dfd557f1aeeb47ccf55208eab4161e4b5bbb
Tom McLaughlin <tom@codedown.io>
2023-04-04 15:28:47 -0700

    Switch from TH to Generics for ToJSON Cookie instance

--- src/Test/WebDriver/Cookies.hs
+++ src/Test/WebDriver/Cookies.hs
@@ -1,32 +1,38 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Test.WebDriver.Cookies where
 
-
 import Data.Aeson
-import Data.Aeson.TH
 import Data.Aeson.Types
 import qualified Data.Char as C
 import Data.Text (Text)
+import GHC.Generics
 import Test.WebDriver.JSON
 
 -- | Cookies are delicious delicacies. When sending cookies to the server, a value
 -- of Nothing indicates that the server should use a default value. When receiving
 -- cookies from the server, a value of Nothing indicates that the server is unable
 -- to specify the value.
-data Cookie = Cookie { cookName   :: Text
-                     , cookValue  :: Text          -- ^
-                     , cookPath   :: Maybe Text    -- ^path of this cookie.
-                                                   -- if Nothing, defaults to /
-                     , cookDomain :: Maybe Text    -- ^domain of this cookie.
-                                                   -- if Nothing, the current pages
-                                                   -- domain is used
-                     , cookSecure :: Maybe Bool    -- ^Is this cookie secure?
-                     , cookExpiry :: Maybe Double  -- ^Expiry date expressed as
-                                                   -- seconds since the Unix epoch
-                                                   -- Nothing indicates that the
-                                                   -- cookie never expires
-                     } deriving (Eq, Show)
+data Cookie = Cookie {
+  cookName   :: Text
+  , cookValue  :: Text          -- ^
+  , cookPath   :: Maybe Text    -- ^path of this cookie.
+                                -- if Nothing, defaults to /
+  , cookDomain :: Maybe Text    -- ^domain of this cookie.
+                                -- if Nothing, the current pages
+                                -- domain is used
+  , cookSecure :: Maybe Bool    -- ^Is this cookie secure?
+  , cookExpiry :: Maybe Double  -- ^Expiry date expressed as
+                                -- seconds since the Unix epoch
+                                -- Nothing indicates that the
+                                -- cookie never expires
+  } deriving (Eq, Show, Generic)
+
+aesonOptionsCookie :: Options
+aesonOptionsCookie = defaultOptions {
+  omitNothingFields = True
+  , fieldLabelModifier = map C.toLower . drop 4
+  }
 
 -- |Creates a Cookie with only a name and value specified. All other
 -- fields are set to Nothing, which tells the server to use default values.
@@ -36,6 +42,9 @@ mkCookie name value = Cookie { cookName = name, cookValue = value,
                                cookSecure = Nothing, cookExpiry = Nothing
                              }
 
+instance ToJSON Cookie where
+  toJSON = genericToJSON aesonOptionsCookie
+  toEncoding = genericToEncoding aesonOptionsCookie
 instance FromJSON Cookie where
   parseJSON (Object o) = Cookie <$> req "name"
                                 <*> req "value"
@@ -49,5 +58,3 @@ instance FromJSON Cookie where
       opt :: FromJSON a => Text -> a -> Parser a
       opt k d = o .:?? k .!= d
   parseJSON v = typeMismatch "Cookie" v
-
-$( deriveToJSON (defaultOptions{omitNothingFields = True, fieldLabelModifier = map C.toLower . drop 4}) ''Cookie )
