1208caf0341cd7eec51114e647bbb94befb84c18
David Hewson <david.hewson@tracsis.com>
2023-12-12 13:20:15 +0000

    file api actually returns the remote file path

--- src/Test/WebDriver/Commands.hs
+++ src/Test/WebDriver/Commands.hs
@@ -680,10 +680,12 @@ setLocation = noReturn . doSessCommand methodPost "/location"
                         "altitude")
 
 -- |Uploads a file from the local filesystem by its file path.
+-- Returns the remote filepath of the file
 uploadFile :: (HasCallStack, WebDriver wd) => FilePath -> wd Text
 uploadFile path = uploadZipEntry =<< liftBase (readEntry [] path)
 
 -- |Uploads a raw bytestring with associated file info.
+-- Returns the remote filepath of the file
 uploadRawFile :: (HasCallStack, WebDriver wd) =>
                  FilePath          -- ^File path to use with this bytestring.
                  -> Integer        -- ^Modification time
@@ -696,6 +698,7 @@ uploadRawFile path t str = uploadZipEntry (toEntry path t str)
 -- |Lowest level interface to the file uploading mechanism.
 -- This allows you to specify the exact details of
 -- the zip entry sent across network.
+-- Returns the remote filepath of the extracted file
 uploadZipEntry :: (HasCallStack, WebDriver wd) => Entry -> wd Text
 uploadZipEntry = doSessCommand methodPost "/file" . single "file"
                  . TL.decodeUtf8 . B64.encode . fromArchive . (`addEntryToArchive` emptyArchive)
