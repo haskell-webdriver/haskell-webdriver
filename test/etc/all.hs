cb518cf62a2ae8b33eb89ad312f161be86c70444
thomasjm <tom@codedown.io>
2024-10-30 04:05:44 -0700

    Remove old QuickCheck test

--- test/etc/all.hs
+++ test/etc/all.hs
@@ -1,11 +1,9 @@
 module Main where
 
 import qualified Async
-import qualified QuickCheck
 import qualified SearchBaidu
 
 main :: IO ()
 main = do
     Async.main
-    QuickCheck.main
     SearchBaidu.main
