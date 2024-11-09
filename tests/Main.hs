0d74cbf331a9d594e9660b91a040721a330c5b69
Tom McLaughlin <tom@mac1.local>
2024-11-09 03:49:18 -0800

    Tests should pass on macOS now

--- tests/Main.hs
+++ tests/Main.hs
@@ -32,7 +32,7 @@ main = do
   let CommandLineWebdriverOptions {..} = optWebdriverOptions clo
 
   runSandwichWithCommandLineArgs' defaultOptions userOptions $
-    introduceNixContext (nixpkgsReleaseDefault { nixpkgsDerivationAllowUnfree = True }) $
+    introduceNixContext (nixpkgsRelease { nixpkgsDerivationAllowUnfree = True }) $
     introduceBinaryViaNixPackage @"java" "jre" $
     (case optSeleniumJar of Just p -> introduceFile @"selenium.jar" p; Nothing -> introduceFileViaNixPackage' @"selenium.jar" "selenium-server-standalone" tryFindSeleniumJar) $
     (case optChromeBinary of Just p -> introduceFile @"google-chrome-stable" p; Nothing -> introduceBinaryViaNixPackage @"google-chrome-stable" "google-chrome") $
@@ -40,6 +40,22 @@ main = do
     introduceBrowserDependencies $
     introduceWebDriver $ do
     Spec.spec
+
+
+-- | Nixpkgs release 24.05, accessed 11\/9\/2024.
+-- You can compute updated values for this release (or others) by running
+-- nix-prefetch-github NixOS nixpkgs --rev release-24.05
+-- We pin this here rather than using 'nixpkgsReleaseDefault' from sandwich-contexts
+-- so that sandwich updates can't break this (especially on aarch64-darwin, where
+-- google-chrome isn't available on older versions of release-24.05).
+nixpkgsRelease :: NixpkgsDerivation
+nixpkgsRelease = NixpkgsDerivationFetchFromGitHub {
+  nixpkgsDerivationOwner = "NixOS"
+  , nixpkgsDerivationRepo = "nixpkgs"
+  , nixpkgsDerivationRev = "bb824c634c812feede9d398c000526401028c0e7"
+  , nixpkgsDerivationSha256 = "sha256-xYnWv9kyJyF8rEZ1uJaSek2fmaIowkk/ovE6+MwcP2E="
+  , nixpkgsDerivationAllowUnfree = False
+  }
 
 
 tryFindSeleniumJar :: FilePath -> IO FilePath
