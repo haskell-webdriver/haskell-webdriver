045ec053e0c6829fa74efc85bb8e75ab5f2502ac
Tom McLaughlin <tom@codedown.io>
2025-02-26 18:44:42 -0800

    Filling in more windows tests

--- tests/Spec/Windows.hs
+++ tests/Spec/Windows.hs
@@ -11,22 +11,26 @@ import TestLib.Types
 
 tests :: SessionSpec
 tests = introduceSession $ describe "Windows" $ before "Open test page" openSimpleTestPage $ do
-  it "getCurrentWindow" $ do
-    pending
+  it "getCurrentWindow / focusWindow" $ do
+    h <- getCurrentWindow
+    info [i|Got current window: #{h}|]
+    focusWindow h
 
   it "getWindowRect" $ do
     rect <- getWindowRect
     info [i|Got rect: #{rect}|]
 
   it "setWindowRect" $ do
-    pending
+    -- TODO: modify the rect
+    getWindowRect >>= setWindowRect
 
   it "getWindowSize" $ do
     size <- getWindowSize
     info [i|Got size: #{size}|]
 
   it "setWindowSize" $ do
-    pending
+    -- TODO: modify the size
+    getWindowSize >>= setWindowSize
 
   it "closeWindow" $ do
-    pending
+    getCurrentWindow >>= closeWindow
