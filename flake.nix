7cc80a6a66e3dae24b6b1acd52ad194e2b426f90
Tom McLaughlin <tom@codedown.io>
2025-02-27 03:49:30 -0800

    Add flake.nix, .envrc, dev-weed alias

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+  inputs.nixpkgsMaster.url = "github:NixOS/nixpkgs/master";
+
+  outputs = { self, flake-utils, nixpkgs, nixpkgsMaster }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pkgsMaster = import nixpkgsMaster { inherit system; };
+
+      in
+        {
+          packages = {
+            inherit (pkgsMaster.haskell.packages.ghc966) weeder;
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+
+            };
+          };
+        }
+    );
+}
