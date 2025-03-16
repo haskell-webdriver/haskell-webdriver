c7e638de5e53e06cc643000e007c3b1ff0d5f722
Tom McLaughlin <tom@codedown.io>
2025-03-16 02:13:41 -0700

    Remove separate Test.Webdriver.Cookies

--- src/Test/WebDriver/Commands/Cookies.hs
+++ src/Test/WebDriver/Commands/Cookies.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 
 module Test.WebDriver.Commands.Cookies (
   cookies
@@ -13,12 +14,14 @@ module Test.WebDriver.Commands.Cookies (
   ) where
 
 import Data.Aeson as A
+import Data.Aeson.Types
 import Data.CallStack
+import qualified Data.Char as C
 import Data.Text (Text)
+import GHC.Generics
 import Prelude -- hides some "unused import" warnings
 import Test.WebDriver.Class
 import Test.WebDriver.Commands.Internal
-import Test.WebDriver.Cookies
 import Test.WebDriver.JSON
 import Test.WebDriver.Utils (urlEncode)
 
@@ -46,3 +49,58 @@ deleteCookieByName n = noReturn $ doSessCommand methodDelete ("/cookie/" <> n) N
 -- | Delete all visible cookies on the current page.
 deleteCookies :: (HasCallStack, WebDriver wd) => wd ()
 deleteCookies = noReturn $ doSessCommand methodDelete "/cookie" Null
+
+-- * Types
+
+-- | Cookies are delicious delicacies. When sending cookies to the server, a value
+-- of Nothing indicates that the server should use a default value. When receiving
+-- cookies from the server, a value of Nothing indicates that the server is unable
+-- to specify the value.
+data Cookie = Cookie {
+  cookName   :: Text
+  , cookValue  :: Text
+  -- | Path of this cookie. If Nothing, defaults to /
+  , cookPath   :: Maybe Text
+  -- | Domain of this cookie. If Nothing, the current page's domain is used.
+  , cookDomain :: Maybe Text
+  -- | Is this cookie secure?
+  , cookSecure :: Maybe Bool
+  -- | Expiry date expressed as seconds since the Unix epoch.
+  -- 'Nothing' indicates that the cookie never expires.
+  , cookExpiry :: Maybe Double
+  } deriving (Eq, Show, Generic)
+
+aesonOptionsCookie :: Options
+aesonOptionsCookie = defaultOptions {
+  omitNothingFields = True
+  , fieldLabelModifier = map C.toLower . drop 4
+  }
+
+-- | Creates a Cookie with only a name and value specified. All other
+-- fields are set to Nothing, which tells the server to use default values.
+mkCookie :: Text -> Text -> Cookie
+mkCookie name value = Cookie {
+  cookName = name
+  , cookValue = value
+  , cookPath = Nothing
+  , cookDomain = Nothing
+  , cookSecure = Nothing
+  , cookExpiry = Nothing
+  }
+
+instance ToJSON Cookie where
+  toJSON = genericToJSON aesonOptionsCookie
+  toEncoding = genericToEncoding aesonOptionsCookie
+instance FromJSON Cookie where
+  parseJSON (Object o) = Cookie <$> req "name"
+                                <*> req "value"
+                                <*> opt "path" Nothing
+                                <*> opt "domain" Nothing
+                                <*> opt "secure" Nothing
+                                <*> opt "expiry" Nothing
+    where
+      req :: FromJSON a => Text -> Parser a
+      req = (o .:) . fromText
+      opt :: FromJSON a => Text -> a -> Parser a
+      opt k d = o .:?? k .!= d
+  parseJSON v = typeMismatch "Cookie" v
