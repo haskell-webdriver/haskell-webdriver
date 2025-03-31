fd8a33d21b642c372d16a438289189c0d1bf64df
Tom McLaughlin <tom@codedown.io>
2025-03-31 13:11:01 -0700

    Add stub test for SeleniumSpecific/Mobile.hs

---
+++ tests/Spec/SeleniumSpecific/Mobile.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Spec.SeleniumSpecific.Mobile where
+
+import Test.Sandwich
+import Test.WebDriver
+import TestLib.Contexts.Session
+import TestLib.Contexts.StaticServer
+import TestLib.Types
+import UnliftIO.Concurrent
+
+
+tests :: SessionSpec
+tests = introduceMobileSession $ describe "Mobile" $ before "Open test page" openSimpleTestPage $ do
+  it "Pauses" $ do
+    threadDelay 1_000_000
+    pending
