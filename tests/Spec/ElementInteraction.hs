0f290ca32f5bb3ba50b9a41dd14e6f98342d71c9
Tom McLaughlin <tom@codedown.io>
2025-05-31 04:54:20 -0700

    Add tests for clearInput, sendKeys

--- tests/Spec/ElementInteraction.hs
+++ tests/Spec/ElementInteraction.hs
@@ -1,7 +1,10 @@
 
 module Spec.ElementInteraction where
 
+import Data.Aeson as A
+import Data.String.Interpolate
 import Test.Sandwich
+import Test.Sandwich.Waits
 import Test.WebDriver.Commands
 import TestLib.Contexts.Session
 import TestLib.Contexts.StaticServer
@@ -20,7 +23,15 @@ tests = introduceSession $ describe "Element interaction" $ before "Open test pa
     findElem (ByCSS "#numberLabel") >>= getText >>= (`shouldBe` "2")
 
   it "clearInput" $ do
-    pending
+    () <- executeJS [] [i|document.querySelector("\#input1").value = "asdf";|]
+    waitUntil 5 $ do
+      findElem (ByCSS "#input1") >>= (`prop` "value") >>= (`shouldBe` (Just (A.String "asdf")))
+
+    findElem (ByCSS "#input1") >>= clearInput
+    waitUntil 5 $ do
+      findElem (ByCSS "#input1") >>= (`prop` "value") >>= (`shouldBe` (Just (A.String "")))
 
   it "sendKeys" $ do
-    pending
+    findElem (ByCSS "#input1") >>= sendKeys "fdsa"
+    waitUntil 5 $ do
+      findElem (ByCSS "#input1") >>= (`prop` "value") >>= (`shouldBe` (Just (A.String "fdsa")))
