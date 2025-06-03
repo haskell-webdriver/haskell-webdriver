02727c378b0db781cf851a58bd84ef7a896d4636
Tom McLaughlin <tom@codedown.io>
2025-06-03 03:57:51 -0700

    Working on modernizing actions

--- src/Test/WebDriver/Commands/Actions.hs
+++ src/Test/WebDriver/Commands/Actions.hs
@@ -1,6 +1,5 @@
-
 module Test.WebDriver.Commands.Actions (
-   moveTo
+  moveTo
   , moveToCenter
   , moveToFrom
   , clickWith
@@ -16,53 +15,16 @@ module Test.WebDriver.Commands.Actions (
 import Data.Aeson as A
 import Data.CallStack
 import Test.WebDriver.Class
-import Test.WebDriver.Commands.ElementRetrieval
 import Test.WebDriver.CommandUtil
 import Test.WebDriver.JSON
-
-
--- | Moves the mouse to the given position relative to the active element.
-moveTo :: (HasCallStack, WebDriver wd) => (Int, Int) -> wd ()
-moveTo = noReturn . doSessCommand methodPost "/moveto" . pair ("xoffset","yoffset")
-
--- | Moves the mouse to the center of a given element.
-moveToCenter :: (HasCallStack, WebDriver wd) => Element -> wd ()
-moveToCenter (Element e) =
-  noReturn . doSessCommand methodPost "/moveto" . single "element" $ e
-
--- | Moves the mouse to the given position relative to the given element.
-moveToFrom :: (HasCallStack, WebDriver wd) => (Int, Int) -> Element -> wd ()
-moveToFrom (x,y) (Element e) =
-  noReturn . doSessCommand methodPost "/moveto"
-  . triple ("element","xoffset","yoffset") $ (e,x,y)
-
--- | Click at the current mouse position with the given mouse button.
-clickWith :: (HasCallStack, WebDriver wd) => MouseButton -> wd ()
-clickWith = noReturn . doSessCommand methodPost "/click" . single "button"
-
--- | Press and hold the left mouse button down. Note that undefined behavior
--- occurs if the next mouse command is not mouseUp.
-mouseDown :: (HasCallStack, WebDriver wd) => wd ()
-mouseDown = noReturn $ doSessCommand methodPost "/buttondown" noObject
-
--- | Release the left mouse button.
-mouseUp :: (HasCallStack, WebDriver wd) => wd ()
-mouseUp = noReturn $ doSessCommand methodPost "/buttonup" noObject
-
--- | Perform the given action with the left mouse button held down. The mouse
--- is automatically released afterwards.
-withMouseDown :: (HasCallStack, WebDriver wd) => wd a -> wd a
-withMouseDown wd = mouseDown >> wd <* mouseUp
-
--- | Double click at the current mouse location.
-doubleClick :: (HasCallStack, WebDriver wd) => wd ()
-doubleClick = noReturn $ doSessCommand methodPost "/doubleclick" noObject
 
 -- | A mouse button
 data MouseButton = LeftButton | MiddleButton | RightButton
   deriving (Eq, Show, Ord, Bounded, Enum)
+
 instance ToJSON MouseButton where
   toJSON = toJSON . fromEnum
+
 instance FromJSON MouseButton where
   parseJSON v = do
     n <- parseJSON v
@@ -71,3 +33,103 @@ instance FromJSON MouseButton where
       1 -> return MiddleButton
       2 -> return RightButton
       err -> fail $ "Invalid JSON for MouseButton: " ++ show err
+
+-- | Execute actions and release them
+executeActions :: (HasCallStack, WebDriver wd) => A.Value -> wd ()
+executeActions actions = do
+  noReturn $ doSessCommand methodPost "/actions" actions
+  -- Release all actions to reset state
+  noReturn $ doSessCommand methodDelete "/actions" noObject
+
+-- | Moves the mouse to the given position relative to the current pointer position.
+moveTo :: (HasCallStack, WebDriver wd) => (Int, Int) -> wd ()
+moveTo (x, y) = executeActions $ createPointerAction "mouse1"
+  [pointerMoveAction (Just "pointer") Nothing x y 250]
+
+-- | Moves the mouse to the center of a given element.
+moveToCenter :: (HasCallStack, WebDriver wd) => Element -> wd ()
+moveToCenter (Element e) = executeActions $ createPointerAction "mouse1"
+  [pointerMoveAction Nothing (Just elementRef) 0 0 250]
+  where
+    elementRef = A.object ["element-6066-11e4-a52e-4f735466cecf" A..= e]
+
+-- | Moves the mouse to the given position relative to the given element.
+moveToFrom :: (HasCallStack, WebDriver wd) => (Int, Int) -> Element -> wd ()
+moveToFrom (x, y) (Element e) = executeActions $ createPointerAction "mouse1"
+  [pointerMoveAction Nothing (Just elementRef) x y 250]
+  where
+    elementRef = A.object ["element-6066-11e4-a52e-4f735466cecf" A..= e]
+
+-- | Click at the current mouse position with the given mouse button.
+clickWith :: (HasCallStack, WebDriver wd) => MouseButton -> wd ()
+clickWith button = executeActions $ createPointerAction "mouse1" [
+  pointerDownAction button
+  , pointerUpAction button
+  ]
+
+-- | Press and hold the left mouse button down. Note that undefined behavior
+-- occurs if the next mouse command is not mouseUp.
+mouseDown :: (HasCallStack, WebDriver wd) => wd ()
+mouseDown = executeActions $ createPointerAction "mouse1"
+  [pointerDownAction LeftButton]
+
+-- | Release the left mouse button.
+mouseUp :: (HasCallStack, WebDriver wd) => wd ()
+mouseUp = executeActions $ createPointerAction "mouse1" [pointerUpAction LeftButton]
+
+-- | Perform the given action with the left mouse button held down. The mouse
+-- is automatically released afterwards.
+withMouseDown :: (HasCallStack, WebDriver wd) => wd a -> wd a
+withMouseDown wd = mouseDown >> wd <* mouseUp
+
+-- | Double click at the current mouse location.
+doubleClick :: (HasCallStack, WebDriver wd) => wd ()
+doubleClick = executeActions $ createPointerAction "mouse1" [
+  pointerDownAction LeftButton
+  , pointerUpAction LeftButton
+  , pauseAction 100
+  , pointerDownAction LeftButton
+  , pointerUpAction LeftButton
+  ]
+
+-- * Internal helpers
+
+createPointerAction :: String -> [A.Value] -> A.Value
+createPointerAction actionId actions = A.object ["actions" A..= [action]]
+  where
+    action = A.object [
+      "type" A..= ("pointer" :: String)
+      , "id" A..= actionId
+      , "actions" A..= actions
+      ]
+
+pointerMoveAction :: Maybe String -> Maybe A.Value -> Int -> Int -> Int -> A.Value
+pointerMoveAction origin element x y duration = A.object $ [
+  "type" A..= ("pointerMove" :: String)
+  , "duration" A..= duration
+  , "x" A..= x
+  , "y" A..= y
+  ] ++ originField
+  where
+    originField = case (origin, element) of
+      (Just o, Nothing) -> ["origin" A..= o]
+      (Nothing, Just e) -> ["origin" A..= e]
+      _ -> []
+
+pointerDownAction :: MouseButton -> A.Value
+pointerDownAction button = A.object [
+  "type" A..= ("pointerDown" :: String)
+  , "button" A..= button
+  ]
+
+pointerUpAction :: MouseButton -> A.Value
+pointerUpAction button = A.object [
+  "type" A..= ("pointerUp" :: String)
+  , "button" A..= button
+  ]
+
+pauseAction :: Int -> A.Value
+pauseAction duration = A.object [
+  "type" A..= ("pause" :: String)
+  , "duration" A..= duration
+  ]
--- tests/Spec/Actions.hs
+++ tests/Spec/Actions.hs
@@ -1,19 +1,33 @@
+{-# LANGUAGE NumericUnderscores #-}
 
 module Spec.Actions where
 
 import Test.Sandwich
+import Test.WebDriver
 import TestLib.Contexts.Session
 import TestLib.Contexts.StaticServer
 import TestLib.Types
+import UnliftIO.Concurrent
 
 
 tests :: SessionSpec
 tests = introduceSession $ describe "Actions" $ before "Open test page" openSimpleTestPage $ do
   it "moveTo" $ pending
-  it "moveToCenter" $ pending
+
+  it "moveToCenter" $ do
+    box <- findElem (ByCSS "#clickable-box")
+    moveToCenter box
+    clickWith LeftButton
+    threadDelay 120_000_000
+
   it "moveToFrom" $ pending
+
   it "clickWith" $ pending
+
   it "mouseDown" $ pending
+
   it "mouseUp" $ pending
+
   it "withMouseDown" $ pending
+
   it "doubleClick" $ pending
