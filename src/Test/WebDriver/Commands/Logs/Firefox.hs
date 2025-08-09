8f3fd668bb8dfe811b3b94e4fb28c96c7abcc5ed
Tom McLaughlin <tom@codedown.io>
2025-08-08 18:19:31 -0700

    Clean up Commands.Logs.* files

--- src/Test/WebDriver/Commands/Logs/Firefox.hs
+++ src/Test/WebDriver/Commands/Logs/Firefox.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Test.WebDriver.Commands.Logs.Firefox (
-  getFirefoxConsoleLogs
-  , getFirefoxLogs
+  getFirefoxLogs
   ) where
 
 import Data.Aeson
@@ -12,42 +11,29 @@ import Test.WebDriver.Commands.Logs.Common
 import Test.WebDriver.JSON
 import Test.WebDriver.Types
 import Test.WebDriver.Util.Commands
-import UnliftIO.Exception (try, SomeException)
+import UnliftIO.Exception
 
--- | Get console logs from Firefox
-getFirefoxConsoleLogs :: (HasCallStack, WebDriver wd) => wd [LogEntry]
-getFirefoxConsoleLogs = getFirefoxLogs "browser"
 
--- | Get logs from Firefox using available methods
 getFirefoxLogs :: (HasCallStack, WebDriver wd) => LogType -> wd [LogEntry]
-getFirefoxLogs logType = do
-  -- Firefox currently has limited logging support in WebDriver
-  -- Try WebDriver BiDi first (when available), then fall back to legacy
-  result <- try $ getFirefoxLogsBiDi logType
-  case result of
+getFirefoxLogs logType =
+  tryAny (getFirefoxLogsBiDi logType) >>= \case
     Right logs -> return logs
-    Left (_ :: SomeException) -> 
-      -- Try legacy endpoint (usually not supported)
-      getLegacyFirefoxLogs logType
+    Left (_ :: SomeException) -> getLegacyFirefoxLogs logType
 
 -- | Attempt to get Firefox logs via WebDriver BiDi (future support)
 getFirefoxLogsBiDi :: (HasCallStack, WebDriver wd) => LogType -> wd [LogEntry]
-getFirefoxLogsBiDi _logType = do
+getFirefoxLogsBiDi _logType =
   -- WebDriver BiDi support for logs is still experimental in Firefox
   -- This would be the future implementation when Firefox supports log.entryAdded
-  result <- doSessCommand methodGet "/log/entries" Null
-  parseFirefoxLogs result
+  doSessCommand methodGet "/log/entries" Null >>= parseFirefoxLogs
 
--- | Parse Firefox logs (when available) 
-parseFirefoxLogs :: WebDriver wd => Value -> wd [LogEntry]  
+parseFirefoxLogs :: WebDriver wd => Value -> wd [LogEntry]
 parseFirefoxLogs (Array logs) = mapM fromJSON' (F.toList logs)
 parseFirefoxLogs _ = return []
 
 -- | Try legacy Firefox log endpoints (typically not supported)
 getLegacyFirefoxLogs :: (HasCallStack, WebDriver wd) => LogType -> wd [LogEntry]
 getLegacyFirefoxLogs logType = do
-  result <- try $ doSessCommand methodPost "/log" $ object ["type" .= logType]
-  case result of  
+  tryAny (doSessCommand methodPost "/log" $ object ["type" .= logType]) >>= \case
     Right (Array logs) -> mapM fromJSON' (F.toList logs)
-    Right _ -> return []
-    Left (_ :: SomeException) -> return [] -- Firefox doesn't support legacy log retrieval
+    _ -> return []
--- src/Test/WebDriver/Commands/Logs/Selenium.hs
+++ src/Test/WebDriver/Commands/Logs/Selenium.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Test.WebDriver.Commands.Logs.Selenium (
-  getSeleniumConsoleLogs
-  , getSeleniumLogs
+  getSeleniumLogs
   ) where
 
 import Data.Aeson
@@ -11,12 +10,7 @@ import Test.WebDriver.Commands.Logs.Common
 import Test.WebDriver.Types
 import Test.WebDriver.Util.Commands
 
--- | Get console logs from Selenium using legacy log endpoint
-getSeleniumConsoleLogs :: (HasCallStack, WebDriver wd) => wd [LogEntry]
-getSeleniumConsoleLogs = getSeleniumLogs "browser"
 
 -- | Get logs from Selenium using the legacy /log endpoint
--- This uses the same endpoint as SeleniumSpecific.Misc.getLogs
--- but is integrated into our modular logging system
 getSeleniumLogs :: (HasCallStack, WebDriver wd) => LogType -> wd [LogEntry]
 getSeleniumLogs logType = doSessCommand methodPost "/log" $ object ["type" .= logType]
